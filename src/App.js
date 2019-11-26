71dc427918eab04ac4fdf0a2e9e4e48022b6f125
Erwin Debusschere <erwincdl@gmail.com>
2019-11-26 01:55:32 -0300

    wip 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ToastProvider, useToasts } from 'react-toast-notifications';
+import {ToastProvider, useToasts} from 'react-toast-notifications';
 
-/*
 const { LiveClient } = require('./pubsub_grpc_web_pb');
 const { SubRequest } = require('./pubsub_pb.js');
-const pubSubService = new LiveClient('http://192.168.1.97:9900');
+const pubSubService = new LiveClient('http://localhost:9900');
 
-const subscribe = (topic, opts) => {
+const subscribe = (topic) => {
 
   const request = new SubRequest();
   request.setName(topic);
@@ -38,6 +37,7 @@ const subscribe = (topic, opts) => {
       this.stream = pubSubService.subscribe(request);
       console.log('stream connected!');
       this.stream.on('data', (data) => this.emit(data));
+      this.stream.on('status', console.log);
       this.stream.on('error', (err) => this.streamErrored(err));
       this.stream.on('end', () => this.cleanupStream());
     },
@@ -46,6 +46,7 @@ const subscribe = (topic, opts) => {
       this.stream = null;
     },
     emit(event) {
+      console.log(event)
       this.streamSucceeded();
       this.getCallbacks().forEach(callback => callback(event));
     },
@@ -68,18 +69,17 @@ const subscribe = (topic, opts) => {
   newSubscription.setupStream();
   return newSubscription;
 };
-*/
 
+/*
 const wsSubscribe = (topic) => {
 
-  const newSocket = () => new WebSocket('ws://192.168.1.97:9900/ws/' + topic);
+  const newSocket = () => new WebSocket('ws://10.0.1.5:9900/ws/' + topic);
 
   const maxRetries = 3;
   const backoff = 2000;
 
   const newSubscription = {
     callbacks: {},
-    stream: newSocket(),
     retryCount: 0,
     streamErrored(error) {
       console.error('stream failed');
@@ -93,7 +93,7 @@ const wsSubscribe = (topic) => {
       }
     },
     setupStream() {
-      if (this.stream) {
+      if (this.stream && this.stream.readyState == 0) {
         this.cleanupStream();
       }
       this.stream = newSocket();
@@ -130,13 +130,14 @@ const wsSubscribe = (topic) => {
   newSubscription.setupStream();
   return newSubscription;
 };
-
-const s2 = wsSubscribe('example');
+ */
+const s2 = subscribe('example');
 
 const Toasts = () => {
-  const { addToast } = useToasts();
+  const {addToast} = useToasts();
   s2.subscribeOnMessage('id', response => {
-    addToast(response.data, { appearance: 'success', autoDismiss: true });
+    const message = response.data || response.getMessage();
+    addToast(message, {appearance: 'success', autoDismiss: true});
   });
   return null;
 };
